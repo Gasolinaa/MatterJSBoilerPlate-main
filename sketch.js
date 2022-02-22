882224525d250e14ad43d36ffc1fc24fc555f721
Gasolinaa <97611283+Gasolinaa@users.noreply.github.com>
2022-02-22 16:00:05 -0300

    amkmnkcwmka nka nk dnk

--- sketch.js
+++ sketch.js
@@ -4,40 +4,52 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var ball;
+var ground;
+var leftSide;
+var rightSide
 
-function preload()
-{
+function preload() {
 
 }
 
 function setup() {
-	createCanvas(800, 700);
-
-
+	createCanvas(1600, 700);
+	rectMode(CENTER)
 	engine = Engine.create();
 	world = engine.world;
 
-	var ball_options ={
+	var ball_options = {
 		isStatic: false,
-		restitution:0.3,
-		friction:0,
-		density:1.2
+		restitution: 0.3,
+		friction: 0,
+		density: 1.2
 	}
-    ball = Bodies.circle(400,200,20,ball_options);
-	World.add(world,ball);
+	ball = Matter.Bodies.circle(400, 200, 20, ball_options);
+	World.add(world, ball);
+	leftSide = new Ground(1100,600,20,120)
+	rightSide = new Ground(1350,600,20,120)
+	ground = new Ground(width / 2, 670, width, 20)
 
 	Engine.run(engine);
-  
+
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+	rectMode(CENTER);
+	background(0);
+	ground.display()
+	leftSide.display()
+	rightSide.display()
+	ellipse(ball.position.x, ball.position.y, 20);
+	drawSprites();
+
 }
 
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85})
+	}
+}
 
 
